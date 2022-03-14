cab9cfd31ce7a7e9d797ee101b16e1913e6d1d3a
jubi in <thetruthrunswild@naver.com>
2022-03-14 11:10:59 +0900

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Dotori = styled.div`
+  font-size: 10rem;
+`;
+
+function App() {
+  return <Dotori>Hello dotori!</Dotori>;
+}
+
+export default App;
