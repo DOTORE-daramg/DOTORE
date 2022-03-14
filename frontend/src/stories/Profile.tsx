408db2dee81d1db63b1e006c8060490a574f5eb7
한지희 <g2hhh2ee@gmail.com>
2022-03-15 02:14:49 +0900

    feat: Profile 컴포넌트 추가

--- frontend/src/stories/Profile.tsx
+++ frontend/src/stories/Profile.tsx
@@ -1,12 +1,56 @@
 // Profile.tsx 
 import React from 'react';
+import styled from "styled-components";
+import { ProfileImg } from './ProfileImg';
 
-interface Props {
+const StyledItem = styled.div`
+    color: pink;
+    font-family: "SUIT", sans-serif;
+    font-weight: 600;
+    border: 2px;
+    border-color: black;
+    border-radius: 10px;
+    display: flex;
+`;
+
+// const StyledProfileImg = styled.img`
+
+// `;
+
+const StyledProfileNickname = styled.span`
+    color: black;
+`;
+
+const StyledProfileLevel = styled.span`
+    color: black;
+`;
+
+interface ProfileProps {
     children?: React.ReactElement | string;
+    profileImgUrl: string;
+    profileNickname: string;
+    profileLevel: string;
+    size?: "small" | "medium" | "large";
+    onClick?: () => void;
 }
 
-function Profile({ children }: Props) {
-    return <button>{children}</button>;
+function Profile({
+    children,
+    profileImgUrl,
+    profileNickname,
+    profileLevel,
+    size: "large",
+    ...props
+}: ProfileProps) {
+    return (
+        <StyledItem>
+            {/* {children} */}
+            {/* <StyledProfileImg src={profileImgUrl} alt="profile" /> */}
+            <ProfileImg />
+            <StyledProfileNickname>{profileNickname}</StyledProfileNickname>
+            <StyledProfileLevel>{profileLevel}</StyledProfileLevel>
+        </StyledItem>
+    )
 }
 
 export default Profile;
