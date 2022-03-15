15f150539bab97da6bc67c57d29d510c47d3498f
한지희 <g2hhh2ee@gmail.com>
2022-03-15 11:29:49 +0900

    refactor: tag 이름 변경 및 style 추가

--- frontend/src/stories/Profile.tsx
+++ frontend/src/stories/Profile.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styled from "styled-components";
 import { ProfileImg } from './ProfileImg';
 
-const StyledItem = styled.div`
+const ProfileContainer = styled.div`
     color: pink;
     font-family: "SUIT", sans-serif;
     font-weight: 600;
@@ -11,6 +11,25 @@ const StyledItem = styled.div`
     border-color: black;
     border-radius: 10px;
     display: flex;
+    justify-content: center;
+    @media screen and (max-width: 768px) {
+    justify-content: left;
+    padding-left: 8vw;
+    }
+`;
+
+const ProfileImage = styled.div``;
+const StyledProfileImg = styled.img<{ size: string }>`
+    border: 2px;
+    border-radius: 400px;
+    width: ${props => props.size};
+    height: ${props => props.size};
+`;
+
+const ProfileDescription = styled.div`
+    display: flex;
+    color: white;
+    justify-items: center;
 `;
 
 // const StyledProfileImg = styled.img`
@@ -34,22 +53,32 @@ interface ProfileProps {
     onClick?: () => void;
 }
 
+interface ProfileImgProps {
+    size: string;
+    profileImgUrl: string;
+}
+
 function Profile({
     children,
-    profileImgUrl,
+    //profileImgUrl,
     profileNickname,
     profileLevel,
-    size: "large",
     ...props
-}: ProfileProps) {
+}: ProfileProps, {
+    size,
+    profileImgUrl,
+}: ProfileImgProps) {
     return (
-        <StyledItem>
+        <ProfileContainer>
+            <StyledProfileImg size={size} src={profileImgUrl}>
+
+            </StyledProfileImg>
             {/* {children} */}
             {/* <StyledProfileImg src={profileImgUrl} alt="profile" /> */}
-            <ProfileImg />
+            {/* <ProfileImg /> */}
             <StyledProfileNickname>{profileNickname}</StyledProfileNickname>
             <StyledProfileLevel>{profileLevel}</StyledProfileLevel>
-        </StyledItem>
+        </ProfileContainer>
     )
 }
 
