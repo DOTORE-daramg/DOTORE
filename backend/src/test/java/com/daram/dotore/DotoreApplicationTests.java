bc34542d1cda18940c4b9da6456269dfdac3aebf
조영운 <dnsldnsl07@naver.com>
2022-03-17 18:32:23 +0900

    fix: 누락된 파일 추가

---
+++ backend/src/test/java/com/daram/dotore/DotoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daram.dotore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
