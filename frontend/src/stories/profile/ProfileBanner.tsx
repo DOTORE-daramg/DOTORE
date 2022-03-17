06005522e2878f578b16631b7c6add6a5dc7695c
한지희 <g2hhh2ee@gmail.com>
2022-03-17 11:40:27 +0900

    [S06P22A407-44] feat: 프로필 컴포넌트 구성

--- frontend/src/stories/profile/ProfileBanner.tsx
+++ frontend/src/stories/profile/ProfileBanner.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from "styled-components";
 import { ProfileImg } from './ProfileImg';
+import dicon from '../assets/mypage/default-dotori-icon.png';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencil } from "@fortawesome/free-solid-svg-icons";
 
 // Banner Container ============================================
 const BannerContainer = styled.div`
@@ -8,7 +11,8 @@ const BannerContainer = styled.div`
     /* height: 100%; */
     position: absolute;
     width: 100%;
-    height: 353px;
+    /* height: 353px; 353-59 = 294 */
+    height: 294px;
     left: 0px;
     top: 92px;
     background: rgba(102, 103, 171, 0.1);
@@ -37,7 +41,12 @@ const ProfileDescriptionContainer = styled.div`
 `;
 const ProfileDescriptionContainerTop = styled.div`
     display: flex;
-    align-items: center
+    align-items: center;
+    cursor: pointer;
+    svg {
+        color: #959595;
+        margin-left: 12px;
+    }
 `;
 const ProfileNickname = styled.span`
     font-size: 20px;
@@ -47,7 +56,7 @@ const ProfileNickname = styled.span`
 `;
 const ProfileLevel = styled.span`
     font-size: 14px;
-    color: #8393AF;
+    color: #4B4B4B;
     font-weight: 400;
 `;
 const ProfileDescriptionContainerBottom = styled.div`
@@ -66,11 +75,18 @@ const ProfileDescription = styled.span`
     font-weight: 500;
     margin-top: 16px;
 `;
-const ProfileDotoriAmount = styled.span`
+const ProfileDotoriAmount = styled.div`
     font-size: 16px;
     color: #515151;
     font-weight: 600;
     margin-top: 16px;
+    display: flex;
+    align-items: center;
+    img {
+        width: 22px;
+        height: 26px;
+        margin-right: 3px;
+    }
 `;
 
 export interface ProfileBannerProps {
@@ -104,11 +120,12 @@ export const ProfileBanner = ({
                     <ProfileDescriptionContainerTop>
                         <ProfileNickname>{profileNickname}</ProfileNickname>
                         <ProfileLevel>{profileLevel}</ProfileLevel>
+                        <FontAwesomeIcon icon={faPencil} />
                     </ProfileDescriptionContainerTop>
                     <ProfileDescriptionContainerBottom>
                         <ProfileAddress>{profileAddress}</ProfileAddress>
                         <ProfileDescription>{profileDescription}</ProfileDescription>
-                        <ProfileDotoriAmount>{profileDotoriAmount}</ProfileDotoriAmount>
+                        <ProfileDotoriAmount><img src={dicon} alt='dicon' />{profileDotoriAmount}</ProfileDotoriAmount>
                     </ProfileDescriptionContainerBottom>
                 </ProfileDescriptionContainer>
             </ProfileContainer>
