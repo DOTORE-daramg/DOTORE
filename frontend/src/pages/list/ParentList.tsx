ad89446935c84de4692288d84518788022289ba8
jubi in <thetruthrunswild@naver.com>
2022-03-17 11:28:58 +0900

    [S06P22A407-64] feat: 분류 컴포넌트 구성

--- frontend/src/pages/list/ParentList.tsx
+++ frontend/src/pages/list/ParentList.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import styled from "styled-components";
+import { InputBox } from "../../stories/InputBox";
+import { Title } from "../../stories/Title";
 const Container = styled.div`
   height: 100vh;
+  margin: 60px;
 `;
+
+const InnerContainer = styled.div``;
 const ParentList = () => {
-  return <Container>PRList</Container>;
+  return (
+    <Container>
+      <Title label="1차 NFT 보기" size="2rem"></Title>
+      <InnerContainer>
+        <InputBox width="10rem" />
+      </InnerContainer>
+    </Container>
+  );
 };
 
 export default ParentList;
---
+++ frontend/src/stories/list/Category.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div<{ isSelected: boolean }>`
+  color: ${(props) => (props.isSelected ? "#6667ab" : "black")};
+  border-bottom: ${(props) =>
+    props.isSelected ? "2px solid #6667ab" : "none"};
+  width: fit-content;
+  font-weight: ${(props) => (props.isSelected ? "600" : "400")}; ;
+`;
+
+interface CatetgoryProps {
+  label: string;
+  isSelected: boolean;
+}
+const Category = ({ label, isSelected }: CatetgoryProps) => {
+  return <Container isSelected={isSelected}>{label}</Container>;
+};
+
+export default Category;
