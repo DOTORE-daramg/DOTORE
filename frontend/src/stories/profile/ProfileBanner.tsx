5df74f9e436cccb805a9e13ee20bf3a424f0a6a9
한지희 <g2hhh2ee@gmail.com>
2022-03-17 10:53:50 +0900

    style: 마이페이지 profile 스타일 변경

--- frontend/src/stories/profile/ProfileBanner.tsx
+++ frontend/src/stories/profile/ProfileBanner.tsx
@@ -28,32 +28,55 @@ const ProfileContainer = styled.div`
 const ProfileImgContainer = styled.div`
     display: flex;
     /* margin: 16px; */
+    margin-right: 62px;
 `;
 // const ProfileImage = styled.img``;
 
 // Profile Container - DescriptionContainer =========
 const ProfileDescriptionContainer = styled.div`
     display: flex;
+    flex-direction: column;
+
 `;
+const ProfileDescriptionContainerTop = styled.div`
+    display: flex;
+    align-items: center
+`;
+
 const ProfileNickname = styled.span`
     font-size: 20px;
-    color: #6667AB;
+    /* color: #6667AB; */
+    color: #000000;
     font-weight: 700;
+    margin-right: 22px;
 `;
 const ProfileLevel = styled.span`
     font-size: 14px;
     color: #8393AF;
     font-weight: 400;
 `;
+
+const ProfileDescriptionContainerBottom = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
 const ProfileAddress = styled.span`
     font-size: 14px;
     color: #959595;
     font-weight: 500;
+    margin-top: 16px;
 `;
 const ProfileDescription = styled.span`
     font-size: 16px;
     color: #4B4B4B;
     font-weight: 500;
+    margin-top: 16px;
+`;
+const ProfileDotoriAmount = styled.span`
+    font-size: 16px;
+    color: #515151;
+    font-weight: 600;
+    margin-top: 16px;
 `;
 
 export interface ProfileBannerProps {
@@ -62,6 +85,7 @@ export interface ProfileBannerProps {
     profileLevel: string;
     profileAddress: string;
     profileDescription: string;
+    profileDotoriAmount: string;
     size: string;
     onClick?: () => void;
 }
@@ -72,6 +96,7 @@ export const ProfileBanner = ({
     profileLevel,
     profileAddress,
     profileDescription,
+    profileDotoriAmount,
     size,
     ...props
 }: ProfileBannerProps) => {
@@ -83,10 +108,15 @@ export const ProfileBanner = ({
                     <ProfileImg size={size} profileImgUrl={profileImgUrl} />
                 </ProfileImgContainer>
                 <ProfileDescriptionContainer>
-                    <ProfileNickname>{profileNickname}</ProfileNickname>
-                    <ProfileLevel>{profileLevel}</ProfileLevel>
-                    <ProfileAddress>{profileAddress}</ProfileAddress>
-                    <ProfileDescription>{profileDescription}</ProfileDescription>
+                    <ProfileDescriptionContainerTop>
+                        <ProfileNickname>{profileNickname}</ProfileNickname>
+                        <ProfileLevel>{profileLevel}</ProfileLevel>
+                    </ProfileDescriptionContainerTop>
+                    <ProfileDescriptionContainerBottom>
+                        <ProfileAddress>{profileAddress}</ProfileAddress>
+                        <ProfileDescription>{profileDescription}</ProfileDescription>
+                        <ProfileDotoriAmount>{profileDotoriAmount}</ProfileDotoriAmount>
+                    </ProfileDescriptionContainerBottom>
                 </ProfileDescriptionContainer>
 
             </ProfileContainer>
