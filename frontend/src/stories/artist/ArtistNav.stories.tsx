015573b1f56c95d3302124a43c17038cb7fd3d42
한지희 <g2hhh2ee@gmail.com>
2022-03-17 19:33:18 +0900

    [S06P22A407-85] feat: 마이페이지 네비게이션 Selected 컨포넌트 구성

--- frontend/src/stories/artist/ArtistNav.stories.tsx
+++ frontend/src/stories/artist/ArtistNav.stories.tsx
@@ -13,29 +13,16 @@ export default {
 
 const Template: ComponentStory<typeof ArtistNav> = (args) => <ArtistNav {...args} />;
 
-export const LoggedIn = Template.bind({});
-LoggedIn.args = {
-    user: {
-        name: "Jane Doe",
-    },
-    isLoggedIn: true,
+export const Selected = Template.bind({});
+Selected.args = {
+    // label: "",
+    isSelected: true,
+    background: "rgba(102, 103, 171, 0.1)",
 };
 
 
-export const LoggedOut = Template.bind({});
-LoggedOut.args = {
-    isLoggedIn: false,
-};
-
-
-
-export const MyPageProfileBannner = Template.bind({});
-MyPageProfileBannner.args = {
-    profileImgUrl: 'https://m.secondmorning.co.kr/file_data/secondmorning/2020/11/11/e712578d88cb3d9ca67bfe33405aee6c.jpg',
-    profileNickname: '주비스',
-    profileLevel: 'Lv.2 꼬맹이도토리',
-    profileAddress: '0x4836680A97a...',
-    profileDescription: `안녕하세요? 주비스입니다? 반갑습니다?`,
-    profileDotoriAmount: `13928 개`,
-    size: '166px',
+export const NotSelected = Template.bind({});
+NotSelected.args = {
+    isSelected: false,
+    background: "none",
 };
