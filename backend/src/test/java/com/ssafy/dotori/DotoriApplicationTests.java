9536d0a1bb4e63892c1a483c4658c495f41e791c
조영운 <dnsldnsl07@naver.com>
2022-03-17 10:52:15 +0900

    [S06P22A407-25] init

---
+++ backend/src/test/java/com/ssafy/dotori/DotoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.dotori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
