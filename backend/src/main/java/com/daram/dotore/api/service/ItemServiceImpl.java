dc3df4f112503c398e740250f48f09f810d9348c
조영운 <dnsldnsl07@naver.com>
2022-03-19 02:09:07 +0900

    [S06P22A407-91] fix: 버그 수정

--- backend/src/main/java/com/daram/dotore/api/service/ItemServiceImpl.java
+++ backend/src/main/java/com/daram/dotore/api/service/ItemServiceImpl.java
@@ -2,8 +2,10 @@ package com.daram.dotore.api.service;
 
 import com.daram.dotore.api.request.ItemReq;
 import com.daram.dotore.db.entity.Items;
+import com.daram.dotore.db.entity.Secondary;
 import com.daram.dotore.db.entity.Taglist;
 import com.daram.dotore.db.repository.ItemRepository;
+import com.daram.dotore.db.repository.SecondaryRepository;
 import com.daram.dotore.db.repository.TagRepository;
 import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,9 @@ public class ItemServiceImpl implements ItemService {
 
     @Autowired
     TagRepository tagRepository;
+
+    @Autowired
+    SecondaryRepository secondaryRepository;
 
     @Override
     public Items saveNewItem(ItemReq itemReq) throws Exception{
@@ -33,16 +38,16 @@ public class ItemServiceImpl implements ItemService {
             .build());
 
         for (String tag : itemReq.getTags()) {
-            Taglist taglist = tagRepository.save(Taglist.builder()
+            tagRepository.save(Taglist.builder()
                 .token_id(itemReq.getToken_id())
                 .tag(tag)
                 .build());
         }
 
-        for (String tag : itemReq.getOriginal()) {
-            Taglist taglist = tagRepository.save(Taglist.builder()
+        for (String ori : itemReq.getOriginal()) {
+            secondaryRepository.save(Secondary.builder()
                 .token_id(itemReq.getToken_id())
-                .tag(tag)
+                .original(ori)
                 .build());
         }
 
