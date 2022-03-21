aec4fddb7a3a3dc4e2499796e7eac403e96efeef
jubi in <thetruthrunswild@naver.com>
2022-03-21 13:53:04 +0900

    fix: amount skeleton 반응형 수정

--- frontend/src/stories/list/ItemSkeleton.tsx
+++ frontend/src/stories/list/ItemSkeleton.tsx
@@ -25,6 +25,9 @@ const AmountContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  @media screen and (max-width: 768px) {
+    width: 50%;
+  }
 `;
 
 interface ItemSkeletonProps {
