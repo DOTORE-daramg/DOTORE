9128b957a56135cbc9eeeb4ec734b1be7ebe0100
이성재 <xpsxm468@naver.com>
2022-03-21 20:42:30 +0900

    feat: artist 페이지 나의 NFT, 창작물 컴포넌트 레이아웃

--- frontend/src/pages/artist/AuthoredNFTList.tsx
+++ frontend/src/pages/artist/AuthoredNFTList.tsx
@@ -1,11 +1,94 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import { ThumbnailGrid } from "../../stories/thumbnail/ThumbnailGrid";
 
-const AuthoredNftList = () => {
+const dummyItemList = [
+  {
+    authorName: '',
+    itemTitle: '',
+    owner_address: '',
+    itemImageUrl: ''
+  },
+  {
+    authorName: '',
+    itemTitle: '',
+    owner_address: '',
+    itemImageUrl: ''
+  },
+  {
+    authorName: '',
+    itemTitle: '',
+    owner_address: '',
+    itemImageUrl: ''
+  },
+  {
+    authorName: '',
+    itemTitle: '',
+    owner_address: '',
+    itemImageUrl: ''
+  },
+  {
+    authorName: '',
+    itemTitle: '',
+    owner_address: '',
+    itemImageUrl: ''
+  },
+  {
+    authorName: '',
+    itemTitle: '',
+    owner_address: '',
+    itemImageUrl: ''
+  },
+  {
+    authorName: '',
+    itemTitle: '',
+    owner_address: '',
+    itemImageUrl: ''
+  },
+  {
+    authorName: '',
+    itemTitle: '',
+    owner_address: '',
+    itemImageUrl: ''
+  },
+  {
+    authorName: '',
+    itemTitle: '',
+    owner_address: '',
+    itemImageUrl: ''
+  },
+  {
+    authorName: '',
+    itemTitle: '',
+    owner_address: '',
+    itemImageUrl: ''
+  },
+]
+
+const Container = styled.div`
+  width: 64rem;
+  display: flex;
+  margin: auto;
+  padding: 5rem 0;
+  justify-content: center;
+  @media screen and (max-width: 1200px) {
+    width: 40rem;
+  }
+  @media screen and (max-width: 500px) {
+    width: 35rem;
+  }
+`;
+
+const AuthoredNFTList = () => {
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <ThumbnailGrid
+        itemList={dummyItemList}
+        size='52rem'
+        columnCount={4}
+      ></ThumbnailGrid>
+    </Container>
   );
 };
 
-export default AuthoredNftList;
+export default AuthoredNFTList;
