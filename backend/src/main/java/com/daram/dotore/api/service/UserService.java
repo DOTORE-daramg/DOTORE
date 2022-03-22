b41ac21bffc978844e59c0a95beb2c1c570d5e6b
조영운 <dnsldnsl07@naver.com>
2022-03-22 23:41:49 +0900

    chore: 파일명 수정

--- backend/src/main/java/com/daram/dotore/api/service/UserService.java
+++ backend/src/main/java/com/daram/dotore/api/service/UserService.java
@@ -3,9 +3,7 @@ package com.daram.dotore.api.service;
 import com.daram.dotore.api.request.DescUpdateReq;
 import com.daram.dotore.api.request.NicknameUpdateReq;
 import com.daram.dotore.api.request.ProfileUpdateReq;
-import com.daram.dotore.db.entity.Feedback;
 import com.daram.dotore.db.entity.Users;
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import java.util.List;
 
 public interface UserService {
