57aeedcbebc5b8761a1f2a479ca834159633f0c9
이호진 <roycmlhj@gmail.com>
2022-03-22 14:53:48 +0900

    feat: : Feedback,Answer jpa 환경

---
+++ backend/src/main/java/com/daram/dotore/db/entity/Answer.java
@@ -0,0 +1,49 @@
+package com.daram.dotore.db.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "answer")
+public class Answer {
+
+    @Id
+    @Column(name = "answerno", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int answerno;
+
+    @NotNull
+    @Column(name = "writer")
+    String writer;
+
+    @NotNull
+    @Column(name = "description")
+    String description;
+
+    @NotNull
+    LocalDateTime created_at;
+
+    String imgUrl;
+
+    @NotNull
+    int articleno;
+
+    @Builder
+    public Answer(int answerno, String writer, String description, LocalDateTime created_at, String imgUrl, int articleno) {
+        this.answerno = answerno;
+        this.writer = writer;
+        this.description = description;
+        this.created_at = created_at;
+        this.imgUrl = imgUrl;
+        this.articleno = articleno;
+    }
+}
