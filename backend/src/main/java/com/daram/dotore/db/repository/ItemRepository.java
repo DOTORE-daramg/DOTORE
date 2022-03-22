fd93624720c577d171245a93923c8c5228d19daf
조영운 <dnsldnsl07@naver.com>
2022-03-22 11:04:39 +0900

    [S06P22A407-103] [S06P22A407-104] feat: 1차, 2차 창작물 조회

--- backend/src/main/java/com/daram/dotore/db/repository/ItemRepository.java
+++ backend/src/main/java/com/daram/dotore/db/repository/ItemRepository.java
@@ -12,6 +12,8 @@ public interface ItemRepository extends JpaRepository<Items, BigInteger> {
 
     Optional<Items> findByTokenId(BigInteger tokenId);
 
+    List<Items> findByIsFirst(boolean isFirst);
+
     @Query(value = "SELECT i.token_id, i.item_hash, i.item_title, i.item_description, i.created_at, i.author_address, i.owner_address, i.on_sale_yn, i.is_first, i.format "
         + "FROM Items i JOIN Secondary s ON i.token_id=s.token_id "
         + "WHERE s.original = :ori", nativeQuery = true)
