4f78c73ba4e59a73c54fa4862a7abda1a88ec8e5
jubi in <thetruthrunswild@naver.com>
2022-03-25 11:33:56 +0900

    feat: add loadspinner

---
+++ frontend/src/stories/common/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import PacmanLoader from "react-spinners/PacmanLoader";
+const LoadingSpinner = () => {
+  return <PacmanLoader color="#6667ab" size={25} />;
+};
+
+export default LoadingSpinner;
