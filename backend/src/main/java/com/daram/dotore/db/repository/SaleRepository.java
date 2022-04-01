17e78ff8f75b91eaae39d6971c8c773995f7a9ed
조영운 <dnsldnsl07@naver.com>
2022-04-02 01:45:14 +0900

    fix: sql 쿼리 error 수정

--- backend/src/main/java/com/daram/dotore/db/repository/SaleRepository.java
+++ backend/src/main/java/com/daram/dotore/db/repository/SaleRepository.java
@@ -36,6 +36,6 @@ public interface SaleRepository extends JpaRepository<Sales, Integer> {
 
     @Query(value = "SELECT * "
         + "FROM Sales "
-        + "WHERE seller_address = :address and sale_id == 0 ", nativeQuery = true)
+        + "WHERE seller_address = :address and sale_id = 0 ", nativeQuery = true)
     List<Sales> getPendingItemList(@Param("address") String address);
 }
