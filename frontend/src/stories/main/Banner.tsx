dcd64926aadd927f689d0c497722a285748d3731
jubi in <thetruthrunswild@naver.com>
2022-04-05 16:42:20 +0900

    style: 배너 애니메이션

--- frontend/src/stories/main/Banner.tsx
+++ frontend/src/stories/main/Banner.tsx
@@ -58,15 +58,21 @@ const ImgContainer = styled.img`
   }
 `;
 
-const MotionTitle = styled(motion.div)`
+const MotionTitle = styled(motion.span)`
   font-weight: 600;
   font-size: 1.8rem;
   color: #585858;
-  /* padding: 20%; */
+  width: 1.6rem;
   line-height: 2;
 `;
 
-const line1 = "우리의 " + "재능이 모여 새로운 가치가 되는 곳,";
+const Name = styled(motion.span)`
+  font-weight: 800;
+  font-size: 2.3rem;
+  color: #6667ab;
+`;
+
+const line1 = "우리의 재능이 모여 새로운 가치가 되는 곳,  ";
 const line2 = "도토리";
 const sentence = {
   hidden: { opacity: 1 },
@@ -86,23 +92,39 @@ const letter = {
     y: 0,
   },
 };
+
 const Banner = () => {
   return (
     <Container variants={sentence} initial="hidden" animate="visible">
-      {line1.split("").map((char, index) => {
-        return (
-          <MotionTitle key={char + "-" + index} variants={letter}>
-            {char}
-          </MotionTitle>
-        );
-      })}
-      {line2.split("").map((char, index) => {
-        return (
-          <MotionTitle key={char + "-" + index} variants={letter}>
-            {char}
-          </MotionTitle>
-        );
-      })}
+      <Content>
+        {line1.split("").map((char, index) => {
+          return (
+            <MotionTitle key={char + "-" + index} variants={letter}>
+              {char}
+            </MotionTitle>
+          );
+        })}
+        {line2.split("").map((char, index) => {
+          console.log(char);
+          return (
+            <Name key={char + "-" + index} variants={letter}>
+              {char}
+            </Name>
+          );
+        })}
+        <div id="desc">
+          안녕하세요! <span id="em">도토리</span>에 오신 걸 환영해요.
+          <br />
+          <span id="em">도토리</span>는 재능 기부를 통한 2차 창작 지원 NFT
+          플랫폼입니다. <br />
+          도네이션 + 토큰 + 리메이크 / 레퍼런스의 줄임말로,
+          <br />
+          재능 기부를 통해 보다 더 많은 창작자들이 NFT 작품을 선보일 수 있게
+          합니다. <br />
+          <div id="last">그렇다면 이제 함께 도토리를 주우러 가 볼까요?</div>
+        </div>
+      </Content>
+      <ImgContainer src={dotori}></ImgContainer>
       {/* <Content>
         우리의 재능이 모여 새로운 가치가 되는 곳,<div id="name">도토리</div>
         <div id="desc">
