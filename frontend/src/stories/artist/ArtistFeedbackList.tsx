767392205982b080a6ad3417c495cace642f03bc
jubi in <thetruthrunswild@naver.com>
2022-04-07 03:18:58 +0900

    fix: articleno 진짜 마지막

--- frontend/src/stories/artist/ArtistFeedbackList.tsx
+++ frontend/src/stories/artist/ArtistFeedbackList.tsx
@@ -5,7 +5,7 @@ import styled from "styled-components";
 import { FeedbackToggleButtons } from "./FeedbackToggleButtons";
 
 export interface IFeedback {
-  articleNo: number;
+  articleno: number;
   yn: boolean;
   itemTitle: string;
   description: string;
@@ -104,7 +104,7 @@ export const ArtistFeedbackList = ({
           </TableBlock>
         </TableHeader>
         {feedbackList.map((feedback) => (
-          <TableRow key={feedback.articleNo}>
+          <TableRow key={feedback.articleno}>
             <TableBlock width="15%">
               <TableCell>{feedback.yn ? "해결" : "미해결"}</TableCell>
             </TableBlock>
@@ -117,7 +117,7 @@ export const ArtistFeedbackList = ({
             </TableBlock>
             <TableBlock
               onClick={() =>
-                navigate(`/feedback/${feedback.tokenId}/${feedback.articleNo}`)
+                navigate(`/feedback/${feedback.tokenId}/${feedback.articleno}`)
               }
               id="link"
               width="33%"
