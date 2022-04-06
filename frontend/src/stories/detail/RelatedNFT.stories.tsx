7752b72b2a45b6990d1c1a5edb7022a79fb643fe
제진명 <1995jjm@naver.com>
2022-04-07 04:30:12 +0900

    fix: 관련작품 스토리북 수정

--- frontend/src/stories/detail/RelatedNFT.stories.tsx
+++ frontend/src/stories/detail/RelatedNFT.stories.tsx
@@ -16,31 +16,4 @@ const Template: ComponentStory<typeof RelatedNFT> = (args) => (
 );
 
 export const Primary = Template.bind({});
-Primary.args = {
-  relatedNFTs: [
-    {
-      itemHash:
-        "https://mblogthumb-phinf.pstatic.net/20150302_49/1eunnue_1425222085350UyECd_JPEG/%BC%D5%B1%D7%B8%B2_%2814%29.jpg?type=w2",
-      itemTitle: "다람쥐",
-      tokenId: "1",
-    },
-    {
-      itemHash:
-        "https://mblogthumb-phinf.pstatic.net/20150302_49/1eunnue_1425222085350UyECd_JPEG/%BC%D5%B1%D7%B8%B2_%2814%29.jpg?type=w2",
-      itemTitle: "다람쥐2",
-      tokenId: "2",
-    },
-    {
-      itemHash:
-        "https://mblogthumb-phinf.pstatic.net/20150302_49/1eunnue_1425222085350UyECd_JPEG/%BC%D5%B1%D7%B8%B2_%2814%29.jpg?type=w2",
-      itemTitle: "다람쥐",
-      tokenId: "3",
-    },
-    {
-      itemHash:
-        "https://mblogthumb-phinf.pstatic.net/20150302_49/1eunnue_1425222085350UyECd_JPEG/%BC%D5%B1%D7%B8%B2_%2814%29.jpg?type=w2",
-      itemTitle: "다람쥐",
-      tokenId: "4",
-    },
-  ],
-};
+Primary.args = {};
